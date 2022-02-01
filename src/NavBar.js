dca4e20396aba36c0604c5a20c1cb9ea01017985
David Verduzco <67717053+tangentdigitalagency@users.noreply.github.com>
2022-02-01 15:42:28 -0700

    Updated Number

--- src/NavBar.js
+++ src/NavBar.js
@@ -15,7 +15,7 @@ class NavBar extends Component {
 
       <ul className="md:flex md:flex-row" id="mobileMenu">
   <li className="pr-5"> 
-    <a className="text-center inline-block px-2 py-2 text-sm text-white font-bold leading-loose bg-blue-400 hover:bg-blue-600 rounded transition duration-200" href="tel:8885671448" data-config-id="04_button">(888) 567-1448</a> 
+    <a className="text-center inline-block px-2 py-2 text-sm text-white font-bold leading-loose bg-blue-400 hover:bg-blue-600 rounded transition duration-200" href="tel:8554761704" data-config-id="04_button">(855) 476-1704</a> 
   </li>  
 
   <li className="inline-block px-2 py-2 text-sm text-gray-500 font-bold leading-loose"> 9AM - 6PM ET Monday - Friday </li>
